f60ce56844f429a67f0af2e7f81645cd825a464b
Adam Marjanovic <80881721+OGADAMo@users.noreply.github.com>
2023-01-18 16:44:36 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,21 @@
+
+const tracks = [
+    {
+        name: "A šta da radim",
+        artist: "Azra",
+        cover: "assets/img/azra.jpg",
+        source: "assets/audio/1.mp3",
+    },
+    {
+        name: "Da raskinem sa njom",
+        artist: "Ceca",
+        cover: "assets/img/ceca.jpg",
+        source: "assets/audio/2.mp3",
+    },
+    {
+        name: "Bježi kišo s prozora",
+        artist: "Crvena Jabuka",
+        cover: "assets/img/crvena.jpg",
+        source: "assets/audio/3.mp3",
+    },
+];
